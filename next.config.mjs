7fcaa9c65bb9c941adb8535784b5170bb77e9647
bataatsets <bataa.tsets@gmail.com>
2024-11-18 12:55:07 +0800

    updated ...

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // output: "export",
+  distDir: process.env.BUILD_DIR,
   images: {
     domains: ["images.unsplash.com", "plus.unsplash.com"],
     unoptimized: true,
