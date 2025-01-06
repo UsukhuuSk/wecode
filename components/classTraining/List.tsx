e9a2d9f7d1241e1377c11479cce50b58999193b5
purevochir <purevochir@smartsolution.mn>
2025-01-06 20:26:15 +0800

    training update

--- components/classTraining/List.tsx
+++ components/classTraining/List.tsx
@@ -32,16 +32,19 @@ const ClassTrainingList = () => {
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
             {
                 list.map((item: any, index: number) => (
-                    <Link key={index} href={`/${locale}/classTraining/${item._id}`}>
-                        <div key={index} className="group relative flex items-center justify-center bg-center bg-contain bg-no-repeat border-neutral-600 hover:border-primary text-white h-80 border  rounded-2xl overflow-hidden"
+                    <Link className={`${index == 0 ? 'row-span-2' : (index === 1 ? 'row-span-2' : 'row-span-1')} ${index === list.length - 1 ? 'col-span-3' : ''}`} key={index} href={`/${locale}/classTraining/${item._id}`}>
+                        <div key={index} className="group relative flex flex-col items-center justify-center bg-center bg-contain bg-no-repeat border-neutral-600 hover:border-primary text-white h-full min-h-80 border  rounded-2xl overflow-hidden"
                         >
                             {
                                 item.image ? <Image width={0}
-                                    height={0} alt="free" className="w-full h-80 absolute object-cover select-none" src={GetFileUrl(item.image._id)} /> : <Image width={0}
-                                        height={0} alt="free" className="w-full h-80 absolute object-cover select-none" src={empAi} />
+                                    height={0} alt="free" className="w-full h-full min-h-80 absolute object-cover select-none" src={GetFileUrl(item.image._id)} /> : <Image width={0}
+                                        height={0} alt="free" className="w-full h-full h-80 absolute object-cover select-none" src={empAi} />
                             }
-                            <div className="group-hover:scale-105 text-center transition-all duration-1000 z-10 top-4 min-h-10 bg-[#000000a6] text-md font-semibold rounded-t-lg rounded-b-md flex items-center p-4 rounded-[2rem] max-w-[90%]">
+                            <div className="group-hover:scale-110 text-center transition-all duration-300 z-10 top-4 min-h-10 text-xl font-semibold rounded-t-lg rounded-b-md flex items-center p-4 rounded-[2rem] max-w-[90%]">
                                 {item.name}
+                            </div>
+                            <div className="group-hover:translate-y-2 z-10 text-center transition-all duration-300">
+                                {item.description.slice(0, 50)}...
                             </div>
                         </div>
                     </Link>
