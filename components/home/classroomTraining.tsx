3bd6face104d6250500d3a7982e7bb19941e39c3
bataatsets <bataa.tsets@gmail.com>
2025-01-29 13:58:24 +0800

    updated text

--- components/home/classroomTraining.tsx
+++ components/home/classroomTraining.tsx
@@ -44,11 +44,12 @@ const ClassroomTraining = () => {
             <div className="flex flex-col items-center justify-center col-span-12 lg:col-span-4 mb-16 md:mb-0">
                 <div className="text-center">
                     <Link href={`/${locale}/classTraining`} className=" font-neue  text-xl font-semibold md:text-5xl md:font-bold max-w-[960px] m-auto  hover:underline text-center">
-                        {isEn() ? 'In-person training registration has started.' : 'Танхимын сургалтын бүртгэл эхэллээ.'}
+                        {/* {isEn() ? 'In-person training registration has started.' : 'Танхимын сургалтын бүртгэл эхэллээ.'} */}
+                        {isEn() ? 'Corporate Training' : 'Байгууллагын Багц'}
                     </Link>
                     <div className="flex justify-center mt-8">
                         <button onClick={handleOpenForm} className="bg-red-500 hover:border border-red-500 hover:bg-transparent hover:text-red-500 hover:scale-110 rounded-[2rem] text-white text-2xl py-2 px-6 font-bold uppercase transition-all">
-                            {isEn() ? 'Register' : 'Бүртгүүлэх'}
+                            {isEn() ? 'Get Started Today' : 'Бидэнтэй холбогдох'}
                         </button>
                     </div>
                 </div>
@@ -58,8 +59,8 @@ const ClassroomTraining = () => {
                 <div className="flex items-center justify-center mb-4 pt-2">
                     <Image className="absolute hidden md:block right-[-150px] top-1/2 -translate-y-[50%] select-none" height={300} alt="In-person training" src={twelve} />
                     <div className="flex flex-col md:flex-row relative items-center gap-2 justify-center">
-                        <div className="bg-gray-50  md:absolute left-0 rounded-xl h-24 w-24 inline-flex justify-center items-center">
-                            <img className="h-20 " alt="In-person training" src={GetFileUrl(course?.logo?._id)} />
+                        <div className="bg-gray-50  md:absolute left-0 rounded-xl h-24 inline-flex justify-center items-center">
+                            <img className="h-24 rounded-xl object-cover" alt="In-person training" src={GetFileUrl(course?.image?._id)} />
                         </div>
                         <p className="w-3/5 text-center text-lg font-bold text-gray-200 ">
                             {course?.name}
