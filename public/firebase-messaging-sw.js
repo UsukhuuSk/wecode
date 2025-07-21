6c12cb2402b56bb0d7172ef9055b4892b764892b
bataatsets <bataa.tsets@gmail.com>
2025-07-21 12:15:39 +0800

    updated ...

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,16 +1,22 @@
 // public/firebase-messaging-sw.js
 importScripts("/firebase-app.js");
 importScripts("/firebase-messaging.js");
-
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBKzwVXqrlf2WBSihtnkqRUjmqhrdt8kqY",
+//   authDomain: "ai-academy-asia.firebaseapp.com",
+//   projectId: "ai-academy-asia",
+//   storageBucket: "ai-academy-asia.firebasestorage.app",
+//   messagingSenderId: "950063556704",
+//   appId: "1:950063556704:web:5217ff3c9d3339a8a5fb05"
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyBKzwVXqrlf2WBSihtnkqRUjmqhrdt8kqY",
-  authDomain: "ai-academy-asia.firebaseapp.com",
-  projectId: "ai-academy-asia",
-  storageBucket: "ai-academy-asia.firebasestorage.app",
-  messagingSenderId: "950063556704",
-  appId: "1:950063556704:web:5217ff3c9d3339a8a5fb05"
+  apiKey: "AIzaSyA_hiL7XrvVCHuST-d4_LdedXR9jxiUHOg",
+  authDomain: "wecodeai.firebaseapp.com",
+  projectId: "wecodeai",
+  storageBucket: "wecodeai.firebasestorage.app",
+  messagingSenderId: "142686461040",
+  appId: "1:142686461040:web:024d01355f25103f62e8b5"
 };
-
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
